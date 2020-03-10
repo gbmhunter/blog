412b3345c60abd0627661cf0d9f1d6aef9854440
Geoffrey Hunter <gbmhunter@gmail.com>
2020-03-10 21:32:02 +1300

    Still working on search functionality.

--- static/js/fastsearch.js
+++ static/js/fastsearch.js
@@ -1,5 +1,5 @@
 var fuse; // holds our search engine
-var searchVisible = false; 
+var searchVisible = false;
 var firstRun = true; // allow us to delay loading json data unless search activated
 var list = document.getElementById('searchResults'); // targets the <ul>
 var first = list.firstChild; // first child of search list
@@ -7,31 +7,51 @@ var last = list.lastChild; // last child of search list
 var maininput = document.getElementById('searchInput'); // input box for search
 var resultsAvailable = false; // Did we get any search results?
 
+$("#fast-search-button").click(function () {
+  console.log('Handler for #fast-search-button called.')
+  if (firstRun) {
+    loadSearch(); // loads our json data and builds fuse.js search index
+    firstRun = false; // let's never do this again
+  }
+
+  // Toggle visibility of search box
+  if (!searchVisible) {
+    $("#fast-search-outer").css("visibility", "visible");
+    document.getElementById("searchInput").focus(); // put focus in input box so you can just start typing
+    searchVisible = true; // search visible
+  }
+  else {
+    document.getElementById("fastSearch").style.visibility = "hidden"; // hide search box
+    document.activeElement.blur(); // remove focus from search box 
+    searchVisible = false; // search not visible
+  }
+});
+
 // ==========================================
 // The main keyboard event listener running the show
 //
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
 
   // CMD-/ to show / hide Search
   if (event.metaKey && event.which === 191) {
-      // Load json search index if first time invoking search
-      // Means we don't load json unless searches are going to happen; keep user payload small unless needed
-      if(firstRun) {
-        loadSearch(); // loads our json data and builds fuse.js search index
-        firstRun = false; // let's never do this again
-      }
+    // Load json search index if first time invoking search
+    // Means we don't load json unless searches are going to happen; keep user payload small unless needed
+    if (firstRun) {
+      loadSearch(); // loads our json data and builds fuse.js search index
+      firstRun = false; // let's never do this again
+    }
 
-      // Toggle visibility of search box
-      if (!searchVisible) {
-        document.getElementById("fastSearch").style.visibility = "visible"; // show search box
-        document.getElementById("searchInput").focus(); // put focus in input box so you can just start typing
-        searchVisible = true; // search visible
-      }
-      else {
-        document.getElementById("fastSearch").style.visibility = "hidden"; // hide search box
-        document.activeElement.blur(); // remove focus from search box 
-        searchVisible = false; // search not visible
-      }
+    // Toggle visibility of search box
+    if (!searchVisible) {
+      document.getElementById("fastSearch").style.visibility = "visible"; // show search box
+      document.getElementById("searchInput").focus(); // put focus in input box so you can just start typing
+      searchVisible = true; // search visible
+    }
+    else {
+      document.getElementById("fastSearch").style.visibility = "hidden"; // hide search box
+      document.activeElement.blur(); // remove focus from search box 
+      searchVisible = false; // search not visible
+    }
   }
 
   // Allow ESC (27) to close search box
@@ -48,8 +68,8 @@ document.addEventListener('keydown', function(event) {
     if (searchVisible && resultsAvailable) {
       console.log("down");
       event.preventDefault(); // stop window from scrolling
-      if ( document.activeElement == maininput) { first.focus(); } // if the currently focused element is the main input --> focus the first <li>
-      else if ( document.activeElement == last ) { last.focus(); } // if we're at the bottom, stay there
+      if (document.activeElement == maininput) { first.focus(); } // if the currently focused element is the main input --> focus the first <li>
+      else if (document.activeElement == last) { last.focus(); } // if we're at the bottom, stay there
       else { document.activeElement.parentElement.nextSibling.firstElementChild.focus(); } // otherwise select the next search result
     }
   }
@@ -58,8 +78,8 @@ document.addEventListener('keydown', function(event) {
   if (event.keyCode == 38) {
     if (searchVisible && resultsAvailable) {
       event.preventDefault(); // stop window from scrolling
-      if ( document.activeElement == maininput) { maininput.focus(); } // If we're in the input box, do nothing
-      else if ( document.activeElement == first) { maininput.focus(); } // If we're at the first item, go to input box
+      if (document.activeElement == maininput) { maininput.focus(); } // If we're in the input box, do nothing
+      else if (document.activeElement == first) { maininput.focus(); } // If we're at the first item, go to input box
       else { document.activeElement.parentElement.previousSibling.firstElementChild.focus(); } // Otherwise, select the search result above the current active one
     }
   }
@@ -69,7 +89,7 @@ document.addEventListener('keydown', function(event) {
 // ==========================================
 // execute search as each character is typed
 //
-document.getElementById("searchInput").onkeyup = function(e) { 
+document.getElementById("searchInput").onkeyup = function (e) {
   executeSearch(this.value);
 }
 
@@ -79,16 +99,16 @@ document.getElementById("searchInput").onkeyup = function(e) {
 //
 function fetchJSONFile(path, callback) {
   var httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = function() {
+  httpRequest.onreadystatechange = function () {
     if (httpRequest.readyState === 4) {
       if (httpRequest.status === 200) {
         var data = JSON.parse(httpRequest.responseText);
-          if (callback) callback(data);
+        if (callback) callback(data);
       }
     }
   };
   httpRequest.open('GET', path);
-  httpRequest.send(); 
+  httpRequest.send();
 }
 
 
@@ -96,8 +116,8 @@ function fetchJSONFile(path, callback) {
 // load our search index, only executed once
 // on first call of search box (CMD-/)
 //
-function loadSearch() { 
-  fetchJSONFile('/index.json', function(data){
+function loadSearch() {
+  fetchJSONFile('/index.json', function (data) {
 
     var options = { // fuse.js options; check fuse.js website for details
       shouldSort: true,
@@ -109,7 +129,7 @@ function loadSearch() {
         'title',
         'permalink',
         'summary'
-        ]
+      ]
     };
     fuse = new Fuse(data, options); // build the index from the json file
   });
@@ -122,6 +142,15 @@ function loadSearch() {
 // in the search box
 //
 function executeSearch(term) {
+  console.log('executeSearch() called with term=' + term)
+
+  // Sometimes during testing/debugging we won't trigger the loadSearch() the normal way, so check
+  // here also
+  if (firstRun) {
+    loadSearch(); // loads our json data and builds fuse.js search index
+    firstRun = false; // let's never do this again
+  }
+
   let results = fuse.search(term); // the actual query being run using fuse.js
   let searchitems = ''; // our results bucket
 
@@ -129,7 +158,7 @@ function executeSearch(term) {
     resultsAvailable = false;
     searchitems = '';
   } else { // build our html 
-    for (let item in results.slice(0,5)) { // only show first 5 results
+    for (let item in results.slice(0, 5)) { // only show first 5 results
       console.log('item=')
       console.log(results[item])
       searchitems = searchitems + '<li><a href="' + results[item].permalink + '" tabindex="0">' + '<span class="title">' + results[item].title + '</span><br />— ' + results[item].date + ' — <em>' + results[item].description + '</em></a></li>';
