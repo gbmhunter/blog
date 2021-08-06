995f68600fab913c0e481b0f8f3e54af6a87a0eb
Geoffrey Hunter <gbmhunter@gmail.com>
2021-08-06 15:38:58 +1200

    Added section on the 'Probabilities' page showing how to generate random numbers that follow a specific distribution.

--- content/mathematics/statistics/probabilities/main.py
+++ content/mathematics/statistics/probabilities/main.py
@@ -4,10 +4,16 @@ from scipy.stats import norm
 
 
 def main():
+    create_pdf_cdf_quantile_plots()
+    generate_rvs_from_pdf()
+
+
+def create_pdf_cdf_quantile_plots():
     fig, ax = plt.subplots()
     x = np.linspace(norm.ppf(0.01),
                     norm.ppf(0.99), 100)
-    ax.plot(x, norm.pdf(x), label='PDF of the normal distribution, $u=0$, $\sigma=1$.')
+    ax.plot(x, norm.pdf(x),
+            label='PDF of the normal distribution, $u=0$, $\\sigma=1$.')
     ax.set_xlabel('$x$')
     ax.set_ylabel('$f(x)$')
     # ax.legend()
@@ -16,7 +22,8 @@ def main():
     fig, ax = plt.subplots()
     x = np.linspace(norm.ppf(0.01),
                     norm.ppf(0.99), 100)
-    ax.plot(x, norm.cdf(x), label='CDF of the normal distribution, $u=0$, $\sigma=1$.')
+    ax.plot(x, norm.cdf(x),
+            label='CDF of the normal distribution, $u=0$, $\\sigma=1$.')
     ax.set_xlabel('$x$')
     ax.set_ylabel('$F(x)$')
     # ax.legend()
@@ -29,6 +36,51 @@ def main():
     ax.set_ylabel('$x$')
     # ax.legend()
     plt.savefig('quantile-normal.png')
+
+
+def generate_rvs_from_pdf():
+
+    from scipy import integrate
+    from scipy.interpolate import interp1d
+    from scipy import stats
+
+    # Make up a example PDF
+    pdf_x = np.linspace(0, np.pi)
+    pdf_y = np.sin(pdf_x)
+    # Normalize pdf_y (make area = 1)
+    pdf_y_interp = interp1d(pdf_x, pdf_y, kind='cubic')
+    integral, _ = integrate.quad(pdf_y_interp, 0, np.pi)
+    pdf_y = pdf_y / integral
+    # Plot this example PDF
+    fig, ax = plt.subplots()
+    ax.scatter(pdf_x, pdf_y)
+    ax.set_xlabel('Random variable, $x$')
+    ax.set_ylabel('Probability $P(x)$')
+    plt.savefig('generating-rvs-starting-pdf.png')
+
+    discrete_cdf1 = integrate.cumtrapz(y=pdf_y, x=pdf_x, initial=0)
+    cdf1 = interp1d(pdf_x, discrete_cdf1)
+    ppf1 = interp1d(discrete_cdf1, pdf_x, bounds_error=False,
+                    fill_value=np.NaN, kind='cubic')
+
+    class Dist(stats.rv_continuous):
+        def _cdf(self, x):
+            return cdf1(x)
+
+        def _ppf(self, x):
+            return ppf1(x)
+
+    dist = Dist(a=pdf_x[0], b=pdf_x[-1], xtol=1e-6)
+
+    # Now generate 100,000 random values that follow the
+    # distribution as specified by your PDF
+    random_values = dist.rvs(size=10000)
+    # Plot these random values
+    fig, ax = plt.subplots()
+    ax.hist(random_values, bins=50)
+    ax.set_xlabel('Random Variable $x$')
+    ax.set_ylabel('Occurrence')
+    plt.savefig('generating-rvs-hist-of-rvs.png')
 
 
 if __name__ == '__main__':
