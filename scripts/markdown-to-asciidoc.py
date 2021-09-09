162240b6fa17faacf43c1141b294d3e052c45ef3
Geoffrey Hunter <gbmhunter@gmail.com>
2021-09-09 20:25:22 +1200

    Converted a number of pages from Markdown to AsciiDoc. Wrote a small Python script to do the grunt work of converting Markdown to AsciiDoc.

---
+++ scripts/markdown-to-asciidoc.py
@@ -0,0 +1,66 @@
+"""
+Tool for doing most of the hard work in converting Markdown to AsciiDoc.
+"""
+import re
+from power_edit import PowerEdit
+
+def main():
+    power_edit = PowerEdit()
+    power_edit.sim_run = False
+    files = power_edit.find_files('../content/electronics/components/crystals-and-oscillators/*.adoc', recursive=True)
+    print(files)
+    for file_path in files:
+        power_edit.find_replace_regex(file_path=file_path, regex_str=r'{{% link[^%]+%}}', replace=link_replace_fn, multiline=True)
+        power_edit.find_replace_regex(file_path=file_path, regex_str=r'{{% (figure|img)[^%]+%}}', replace=image_replace_fn, multiline=True)
+
+def link_replace_fn(found_text, file_path):
+    print(file_path)
+    print(f'found_text = {found_text}')
+
+    # Extract text
+    match = re.search(r'text="([^"]+)"', found_text)
+    text = match.group(1)
+    print(f'text={text}')
+
+    # Extract link
+    match = re.search(r'src="([^"]+)"', found_text)
+    src = match.group(1)
+    print(f'src={src}')
+
+    asciidoc_link = f'link:{src}[{text}]'
+    print(f'asciidoc_link={asciidoc_link}')
+
+    return asciidoc_link
+
+def image_replace_fn(found_text, file_path):
+    print(file_path)
+    print(f'found_text = {found_text}')
+
+    # Extract src
+    match = re.search(r'src="([^"]+)"', found_text)
+    src = match.group(1)
+    print(f'src={src}')
+
+    # Extract width
+    match = re.search(r'width="([^"]+)"', found_text)
+    width = match.group(1)
+    print(f'width={width}')
+
+    # Extract caption
+    match = re.search(r'caption="([^"]+)"', found_text)
+    if match is not None:
+        caption = match.group(1)
+    else:
+        caption = None
+    print(f'caption={caption}')
+
+    asciidoc_image = ''
+    if caption is not None:
+        asciidoc_image += f'.{caption}\n'
+    asciidoc_image = f'image::{src}[width={width}]'
+
+    print(f'asciidoc_image={asciidoc_image}')
+    return asciidoc_image
+
+if __name__ == '__main__':
+    main()
