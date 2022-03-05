812579bf9f7bca7fc34a112899232b903a0901ae
Geoffrey Hunter <gbmhunter@gmail.com>
2022-03-05 20:37:10 +1300

    Added a photo of a Traco PSU.

--- content/electronics/components/diodes/general-purpose-diodes/main.py
+++ content/electronics/components/diodes/general-purpose-diodes/main.py
@@ -6,11 +6,26 @@ from pathlib import Path
 def main():
     script_dir = Path(os.path.dirname(os.path.realpath(__file__)))
 
-    t = np.linspace(0, np.pi*2)
+    t = np.linspace(0, np.pi*2, num=500)
     i_max_A = 5
     y = i_max_A * np.sin(t)
     fig, ax = plt.subplots()
-    ax.plot(t, y)
+    ax.plot(t, y, label='Instantaneous Diode Current')
+    bool_filter = y >= 0
+    ax.fill_between(t[bool_filter], y[bool_filter], alpha=0.5, label='Period Of Diode Conduction')
+
+    # Draw horizontal lines at max. and average current
+    ax.axhline(y=i_max_A, c='C1', ls='--')
+    ax.text(x=1, y=i_max_A + 0.1, s='$I_{max}$')
+    avg_i_A = i_max_A/np.pi
+    ax.axhline(y=avg_i_A, c='C1', ls='--')
+    ax.text(x=1, y=avg_i_A + 0.1, s='$I_{avg} = \\frac{I_{max}}{\pi}$')
+    ax.grid()
+    ax.set_ylim(-6, 6)
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel('Current [A]')
+    ax.set_title('Graph Showing Relationship Between Diode Average Forward Current\nAnd Max. Current')
+    ax.legend()
     plt.savefig(script_dir / 'avg-rectified-forward-current-plot.png')
 
 if __name__ == '__main__':
