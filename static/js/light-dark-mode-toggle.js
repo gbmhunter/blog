f4d386197c5eca23363f1bc83c20a27dcaeeb6c5
Geoffrey Hunter <gbmhunter@gmail.com>
2023-12-09 21:16:48 +1300

    Working on adding top navbar and dark theme.

---
+++ static/js/light-dark-mode-toggle.js
@@ -0,0 +1,18 @@
+var toggle = document.getElementById("theme-toggle");
+
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
+
+toggle.onclick = function() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
