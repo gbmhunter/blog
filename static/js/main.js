596bfd21697c3a52b2f0369dc0acfb01dbde14f2
Geoffrey Hunter <gbmhunter@gmail.com>
2023-12-11 08:53:19 +1300

    Improved the scroll to top button.

--- static/js/main.js
+++ static/js/main.js
@@ -2,12 +2,13 @@
 // Based of code at https://codepen.io/rdallaire/pen/apoyx
 $(window).scroll(function() {
   if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-      $('#return-to-top').fadeIn(200);    // Fade in the arrow
+      $('.return-to-top').fadeIn(200);    // Fade in the arrow
   } else {
-      $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+      $('.return-to-top').fadeOut(200);   // Else fade out the arrow
   }
 });
-$('#return-to-top').click(function() {      // When arrow is clicked
+
+$('.return-to-top').click(function() {      // When arrow is clicked
   $('body,html').animate({
       scrollTop : 0                       // Scroll to top of body
   }, 500);
