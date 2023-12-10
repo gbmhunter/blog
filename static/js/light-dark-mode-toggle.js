707da8d03f7a604c390223aa66ffb7c9dc2ea0cf
Geoffrey Hunter <gbmhunter@gmail.com>
2023-12-10 17:01:55 +1300

    Fixed white flash issue with dark mode.

--- static/js/light-dark-mode-toggle.js
+++ static/js/light-dark-mode-toggle.js
@@ -1,11 +1,15 @@
-var toggle = document.getElementById("theme-toggle");
 
 var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-if (storedTheme)
-    document.documentElement.setAttribute('data-theme', storedTheme)
+if (storedTheme) {
+  document.documentElement.setAttribute('data-theme', storedTheme);
+} else {
+  document.documentElement.setAttribute('data-theme', 'light');
+}
 
-
-toggle.onclick = function() {
+// Only run this when DOM has loaded, otherwise it will not find the element
+window.addEventListener("DOMContentLoaded", function() {
+  var toggle = document.getElementById("theme-toggle");
+  toggle.onclick = function() {
     var currentTheme = document.documentElement.getAttribute("data-theme");
     var targetTheme = "light";
 
@@ -15,4 +19,6 @@ toggle.onclick = function() {
 
     document.documentElement.setAttribute('data-theme', targetTheme)
     localStorage.setItem('theme', targetTheme);
-};
+  };
+}, false);
+
