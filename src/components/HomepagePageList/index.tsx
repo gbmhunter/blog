dfc1e863bff7064e9fc5fc54b8869280a1815a6a
Geoffrey Hunter <gbmhunter@gmail.com>
2024-07-24 21:55:17 +1200

    Add DocCardLists to more pages.

--- src/components/HomepagePageList/index.tsx
+++ src/components/HomepagePageList/index.tsx
@@ -1,37 +1,20 @@
 import React from 'react';
-import {useDocsSidebar} from '@docusaurus/theme-common/internal';
-// import {getSidebarBreadcrumbs} from '@docusaurus/theme-common';
+import {useDocsSidebar, useDocById} from '@docusaurus/theme-common/internal';
 import DocCardList from '@theme/DocCardList';
 
 export default function HomepageFeatures(): JSX.Element {
-
-  // const {pathname} = useLocation();
-  const pathname = '/electronics/';
   const sidebar = useDocsSidebar();
-  console.log('sidebar', sidebar);
+  console.log('sidebar:', sidebar);
 
+  const doc = useDocById('electronics/low-power-design/index');
+  console.log('doc:', doc);
+
+  // Remove items that are hidden with sidebar_class_name: hidden in the frontmatter
   const itemsToShow = sidebar.items.filter((item) => {
     return item.className !== 'hidden';
   });
-  // if (!sidebar) {
-  //   throw new Error('Unexpected: cant find current sidebar in context');
-  // }
-  // const categoryBreadcrumbs = getSidebarBreadcrumbs({
-  //   sidebarItems: sidebar.items,
-  //   pathname,
-  //   onlyCategories: true,
-  // });
-  // const deepestCategory = categoryBreadcrumbs.slice(-1)[0];
-  // if (!deepestCategory) {
-  //   throw new Error(
-  //     `${pathname} is not associated with a category. useCurrentSidebarCategory() should only be used on category index pages.`,
-  //   );
-  // }
-  // return deepestCategory;
 
-
-
-    return (
-      <DocCardList items={itemsToShow} />
-    );
+  return (
+    <DocCardList items={itemsToShow} />
+  );
 }
