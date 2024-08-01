3f4971a924f3a0b25969614a042a8e5f61096e1e
Geoffrey Hunter <gbmhunter@gmail.com>
2024-08-01 20:03:26 +1200

    Start adding dynamic routing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -229,19 +229,6 @@ export default defineConfig({
           },
         },
       })],
-      // sidebar: [
-      //   {
-      //     label: "Guides",
-      //     items: [
-      //       // Each item here is one entry in the navigation menu.
-      //       { label: "Example Guide", slug: "guides/example" },
-      //     ],
-      //   },
-      //   {
-      //     label: "Reference",
-      //     autogenerate: { directory: "reference" },
-      //   },
-      // ],
       sidebar: sidebarNodes.items,
     }),
   ],
