a507d9d9de248077972727325c274eaf56b9ce0b
Geoffrey Hunter <gbmhunter@gmail.com>
2024-08-15 15:36:24 +1200

    Fix Aside CSS bugs. Remove unused npm dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,38 +2,10 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
-import rehypeMathJax from "rehype-mathjax";
-import AutoImport from "astro-auto-import";
-import starlightBlog from 'starlight-blog'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    AutoImport({
-      imports: [
-        // Import a component’s default export
-        // generates:
-        // import A from './src/components/A.astro';
-        "./src/components/A.astro",
-
-        {
-          // Explicitly alias a default export
-          // generates:
-          // import { default as B } from './src/components/B.astro';
-          "./src/components/B.astro": [["default", "B"]],
-
-          // Import a module’s named exports
-          // generates:
-          // import { Tweet, YouTube } from 'astro-embed';
-          "astro-embed": ["Tweet", "YouTube"],
-
-          // Import all named exports from a module as a namespace
-          // generates:
-          // import * as Components from './src/components';
-          "./src/components": "Components",
-        },
-      ],
-    }),
     // Make sure the MDX integration is included AFTER astro-auto-import
     starlight({
       title: 'mbedded.ninja',
