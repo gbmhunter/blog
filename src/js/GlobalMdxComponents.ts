7a59ab2f741f8c4c57b7f1fd7a9553504dc4c82f
Geoffrey Hunter <gbmhunter@gmail.com>
2025-01-04 09:38:26 +1300

    Fix bug with IRef not working on frontpage.

--- src/js/GlobalMdxComponents.ts
+++ src/js/GlobalMdxComponents.ts
@@ -2,7 +2,10 @@ import Aside from 'src/components/Aside.astro';
 import ChildPages from 'src/components/ChildPages.astro';
 import CircuitJs from 'src/components/CircuitJs.astro';
 import Image from 'src/components/Image.astro';
+import IRef from 'src/components/IRef.astro';
+import Table from 'src/components/Table.astro';
 import WarningIsNotes from 'src/components/WarningIsNotes.astro';
+import WrappableRow from 'src/components/WrappableRow.astro';
 
 export function getGlobalMdxComponents() {
   return {
@@ -10,6 +13,9 @@ export function getGlobalMdxComponents() {
     ChildPages: ChildPages,
     CircuitJs: CircuitJs,
     Image: Image,
-    WarningIsNotes: WarningIsNotes
+    IRef: IRef,
+    Table: Table,
+    WarningIsNotes: WarningIsNotes,
+    WrappableRow: WrappableRow,
   }
 }
