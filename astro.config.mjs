b8896df038ced881d84aaea9a1de999c2d006e06
Geoffrey Hunter <gbmhunter@gmail.com>
2025-03-03 20:00:33 +1300

    Add nRF52 GPIO info.

--- astro.config.mjs
+++ astro.config.mjs
@@ -48,7 +48,7 @@ export default defineConfig({
             rel: 'stylesheet',
             href: 'https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css',
           },
-        }
+        },
       ],
     }),
   ],
