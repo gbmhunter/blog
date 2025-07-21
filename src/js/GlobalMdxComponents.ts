6a032fd993a0ec812bb976b8830943a5afb87a15
Geoffrey Hunter <gbmhunter@gmail.com>
2025-07-22 07:58:43 +1200

    Tidy up astro component imports.

--- src/js/GlobalMdxComponents.ts
+++ src/js/GlobalMdxComponents.ts
@@ -1,3 +1,7 @@
+/**
+ * This file is used to consolidate all the global MDX components into a single object. [...slug].astro imports this
+ * and provides the components to the MDX renderer.
+ */
 import Aside from 'src/components/Aside.astro';
 import ChildPages from 'src/components/ChildPages.astro';
 import CircuitJs from 'src/components/CircuitJs.astro';
@@ -6,6 +10,7 @@ import IRef from 'src/components/IRef.astro';
 import Table from 'src/components/Table.astro';
 import WarningIsNotes from 'src/components/WarningIsNotes.astro';
 import WrappableRow from 'src/components/WrappableRow.astro';
+import Video from 'src/components/Video.astro';
 
 export function getGlobalMdxComponents() {
   return {
@@ -17,5 +22,6 @@ export function getGlobalMdxComponents() {
     Table: Table,
     WarningIsNotes: WarningIsNotes,
     WrappableRow: WrappableRow,
+    Video: Video,
   }
 }
