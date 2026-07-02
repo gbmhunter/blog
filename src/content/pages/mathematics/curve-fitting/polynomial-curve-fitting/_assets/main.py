9ce9013a7ac755a86da6ec8dd3521a38353b2115
Geoffrey Hunter <gbmhunter@gmail.com>
2026-07-02 21:35:28 +1200

    Add info on LED pixel displays.

--- src/content/pages/mathematics/curve-fitting/polynomial-curve-fitting/_assets/main.py
+++ src/content/pages/mathematics/curve-fitting/polynomial-curve-fitting/_assets/main.py
@@ -1,118 +1,111 @@
-#!/usr/bin/env python3
+# /// script
+# requires-python = ">=3.10"
+# dependencies = [
+#     "matplotlib",
+#     "numpy",
+# ]
+# ///
+from pathlib import Path
 
 import matplotlib.pyplot as plt
-plt.switch_backend('agg') # Allow matplotlib to work in Docker
-
 import numpy as np
 
-def calc_tri(max_num, index):
-    """
-    Calculates the triangular number given the maximum number and
-    the current index.
-    e.g. calc_tri(3, i) for i = 0..8 would give:
-    0, 1, 2, 3, 2, 1, 0, 1, 2
-    """
-    print(f'calc_tri() called with max_num = {max_num}, index = {index}.')
-    period = max_num * 2
-    print(f'period = {period}')
+SCRIPT_DIR = Path(__file__).parent
 
-    mod = index % period
-    print(f'mod = {mod}')
+X_DATA = np.array([1, 2, 3, 4])
+Y_DATA = np.array([1, 3, 4, 8])
 
 
-    if mod <= max_num:
-        tri = mod
-    else:
-        tri = max_num - (mod - max_num)
-
-    print(f'tri = {tri}')
+def main():
+    create_many_polynomial_best_fits_plot()
+    create_2_degree_poly_best_fit_plot()
 
 
 def find_best_fit_poly(x_data, y_data, poly_degree):
-    # First co-efficient is a0*x^0, second is a1*x^1, e.t.c
-    poly_coefficients = np.zeros(poly_degree + 1)
-    
-    # POPULATE MATRIX A
-    matrix_A = np.zeros([poly_coefficients.size, poly_coefficients.size])
+    """
+    Finds the least-squares polynomial of best fit by building the A and B
+    matrices and solving Ax = B (as described on the page).
+    Returns the coefficients [a0, a1, ..., ak] as a column vector.
+    """
+    num_coefficients = poly_degree + 1
 
-    for i in range(poly_coefficients.size*2 - 1):
-        sum = (x_data**i).sum()
+    # POPULATE MATRIX A
+    matrix_A = np.zeros([num_coefficients, num_coefficients])
+
+    for i in range(num_coefficients * 2 - 1):
+        power_sum = (x_data**i).sum()
 
         # Sum is inserted on a trailing diagonal
         # All row, col pairs that add to i are valid
         curr_col = i
         curr_row = 0
-        if curr_col > poly_coefficients.size - 1:
-            curr_col = poly_coefficients.size - 1
+        if curr_col > num_coefficients - 1:
+            curr_col = num_coefficients - 1
             curr_row = i - curr_col
 
         while True:
-            matrix_A[curr_row, curr_col] = sum
+            matrix_A[curr_row, curr_col] = power_sum
             curr_col -= 1
             curr_row += 1
 
-            if curr_col < 0 or curr_row > poly_coefficients.size - 1:
+            if curr_col < 0 or curr_row > num_coefficients - 1:
                 break
 
-    print(f'matrix_A = {matrix_A}')
-
     # POPULATE MATRIX B
-    matrix_B = np.zeros([poly_coefficients.size, 1])
+    matrix_B = np.zeros([num_coefficients, 1])
 
-    for i in range(poly_coefficients.size):
-        print(x_data**i)
-        sum = ((x_data**i)*y_data).sum()
-        print(f'i = {i}, sum = {sum}')
-        matrix_B[i, 0] = sum
+    for i in range(num_coefficients):
+        matrix_B[i, 0] = ((x_data**i) * y_data).sum()
 
-    print(f'matrix_B = {matrix_B}')
-
-    # Solve for x. Ax = B so x = A-1*B
-    x = np.dot(np.linalg.inv(matrix_A), matrix_B)
-
-    print(f'x = {x}')
-    return x
+    # Solve for x. Ax = B so x = A^-1 * B
+    return np.dot(np.linalg.inv(matrix_A), matrix_B)
 
 
-# ============================================= #
-# =================== INPUTS ================== #
-# ============================================= #
-x_data = np.array([ 1, 2, 3, 4])
-y_data = np.array([ 1, 3, 4, 8])
+def eval_poly(coefficients, x):
+    y = np.zeros(x.size)
+    for i in range(coefficients.size):
+        y += (x**i) * coefficients[i]
+    return y
 
-# A list of all the degrees of the polynomials that will be drawn on the
-# graph
-poly_degrees = np.array([ 0, 1, 2, 3 ])
 
-poly_coefficients = []
-for i in range(poly_degrees.size):
-    poly_coefficients.append(find_best_fit_poly(x_data, y_data, poly_degrees[i]))
+def create_many_polynomial_best_fits_plot():
+    poly_degrees = [0, 1, 2, 3]
 
-print(poly_coefficients)
+    fig, ax = plt.subplots()
+    ax.scatter(X_DATA, Y_DATA, color='green', label='Data points', zorder=3)
 
-# ============================================= #
-# ================== PLOTTING ================= #
-# ============================================= #
-fig, ax = plt.subplots()
+    best_fit_x = np.linspace(X_DATA.min() - 0.1, X_DATA.max() + 0.1, num=100)
+    for poly_degree in poly_degrees:
+        coefficients = find_best_fit_poly(X_DATA, Y_DATA, poly_degree)
+        ax.plot(best_fit_x, eval_poly(coefficients, best_fit_x),
+                label=f'Degree-{poly_degree} polynomial')
 
-# Plot points
-points = ax.scatter(x_data, y_data, color='green', label='Data points')
+    ax.set_title('Polynomial Curve Fitting\n(Least Squares Approach)')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.grid()
+    ax.legend()
+    fig.tight_layout()
+    plt.savefig(SCRIPT_DIR / 'many-polynomial-best-fits-on-graph.png')
 
-bestFitX = np.linspace(x_data.min() - 0.1, x_data.max() + 0.1, num=100)
 
-# Plot best fit polynomials
-best_fit_polys = []
-for poly_coefficient in poly_coefficients:
-    
-    bestFitY = np.zeros(bestFitX.size)
-    for i in range(poly_coefficient.size):
-        bestFitY += (bestFitX**i)*poly_coefficient[i]
+def create_2_degree_poly_best_fit_plot():
+    fig, ax = plt.subplots()
+    ax.scatter(X_DATA, Y_DATA, color='green', label='Data points', zorder=3)
 
-    best_fit, = ax.plot(bestFitX, bestFitY, label=f'{i} degree polynomial')
-    best_fit_polys.append(best_fit)
+    best_fit_x = np.linspace(X_DATA.min() - 0.1, X_DATA.max() + 0.1, num=100)
+    coefficients = find_best_fit_poly(X_DATA, Y_DATA, 2)
+    ax.plot(best_fit_x, eval_poly(coefficients, best_fit_x),
+            label='Degree-2 polynomial')
 
-plt.title('Polynomial Curve Fitting\n(Least Squares Approach)')
-plt.xlabel('x')
-plt.ylabel('y')
-ax.legend()
-plt.savefig('many-polynomial-best-fits-on-graph.png')
+    ax.set_title('Polynomial Curve Fitting\n(Least Squares Approach)')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.grid()
+    ax.legend()
+    fig.tight_layout()
+    plt.savefig(SCRIPT_DIR / '2-degree-poly-best-fit-graph.png')
+
+
+if __name__ == '__main__':
+    main()
